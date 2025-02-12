ff0364fc154843bf9ef8ad455dbdc1d2c042ce23
Felix <felix_gilioli@hotmail.com>
2025-02-12 10:26:43 -0300

    update groupid

--- src/main/java/br/com/felixgilioli/bff/config/EnableTemplateEngine.java
+++ src/main/java/br/com/felixgilioli/bff/config/EnableTemplateEngine.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@ComponentScan("io.github.felixgilioli.bff")
+@ComponentScan("br.com.felixgilioli.bff")
 public @interface EnableTemplateEngine {
 }
