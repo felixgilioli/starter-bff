730855eb1a5d0d8e265b3c62fa0df030a8cddb41
Felix <felix_gilioli@hotmail.com>
2025-06-23 10:19:25 -0300

    images exames

--- src/main/java/br/com/felixgilioli/bff/doc/ResponseExampleDescriptionFormatter.java
+++ src/main/java/br/com/felixgilioli/bff/doc/ResponseExampleDescriptionFormatter.java
@@ -3,6 +3,8 @@ package br.com.felixgilioli.bff.doc;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -12,31 +14,42 @@ public class ResponseExampleDescriptionFormatter {
     @Value("${server.servlet.context-path:}")
     private String contextPath;
 
+    private static <T> List<List<T>> partition(List<T> list, int size) {
+        List<List<T>> partitions = new ArrayList<>();
+        for (int i = 0; i < list.size(); i += size) {
+            partitions.add(list.subList(i, Math.min(i + size, list.size())));
+        }
+        return partitions;
+    }
+
     public Optional<String> format(DocumentationData<?> documentationData) {
-        if (documentationData.getExampleImages() == null || documentationData.getExampleImages().isEmpty()) {
+        List<DocumentationExampleImage> images = documentationData.getExampleImages();
+        if (images == null || images.isEmpty()) {
             return Optional.empty();
         }
 
-        var imagesHTML = documentationData.getExampleImages()
-                .stream()
-                .map(img -> """
+        var rowsHtml = partition(images, 3).stream()
+                .map(group -> group.stream()
+                        .map(img -> """
                         <td>
                             %s<br>
                             <img src="%s" alt="%s" width="300"/>
                         </td>
                         """.formatted(
-                        img.getDescription(),
-                        getImageSrc(img.getSrc()),
-                        img.getDescription()
-                ))
+                                        img.getDescription(),
+                                        getImageSrc(img.getSrc()),
+                                        img.getDescription()
+                                )
+                        ).collect(Collectors.joining())
+                )
+                .map(tds -> "<tr>" + tds + "</tr>")
                 .collect(Collectors.joining());
 
         var descriptionHTML = """
-                <table>
-                    <tr>
-                        %s
-                </table>
-                """.formatted(imagesHTML); // unable to close tr tag due to swagger bug
+            <table>
+                %s
+            </table>
+            """.formatted(rowsHtml);
 
         return Optional.of(descriptionHTML);
     }
